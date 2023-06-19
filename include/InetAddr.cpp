2a16ffd9095ce5af49cb5ea446ef252a64e9c61b
lostprobe99 <lostprobe@qq.com>
2023-06-19 18:15:13 +0800

    为InetAddr添加<<,to_string和地址，端口访问函数

--- include/InetAddr.cpp
+++ include/InetAddr.cpp
@@ -1,6 +1,21 @@
 #include "InetAddr.h"
+#include "debug.h"
+#include <netdb.h>
 #include <cstring>
 
+std::string to_string(const InetAddr& value)
+{
+    const char * fmt = "InetAddr{ addr = \"%s\", port = %u }";
+    char buf[64]{};
+    snprintf(buf, 64, fmt, value.addr().c_str(), value.port());
+    return (buf);
+}
+
+std::ostream& operator<<(std::ostream& os, const InetAddr& rhs)
+{
+    os << to_string(rhs);
+    return os;
+}
 
 InetAddr::InetAddr() : _addr_len(sizeof(_addr))
 {
@@ -11,9 +26,57 @@ InetAddr::InetAddr(const char * addr, uint16_t port)
 {
     bzero(&_addr, sizeof(_addr));
     _addr.sin_family = AF_INET;
-    _addr.sin_addr.s_addr = inet_addr(addr);
+    _addr.sin_addr.s_addr = (INADDR_ANY);
     _addr.sin_port = htons(port);
     _addr_len = sizeof(_addr);
+    if(addr != NULL)
+    {
+        struct hostent* h = gethostbyname(addr);
+        if(h == NULL)
+        {
+            FATAL("addr = `%s` error", addr);
+            exit(1);
+        }
+        this->_addr.sin_addr = *((struct in_addr*)(h->h_addr_list[0]));
+    }
+}
+
+std::string InetAddr::addr() const
+{
+    std::string ip = inet_ntoa(this->_addr.sin_addr);
+    return ip;
+}
+
+void InetAddr::addr(std::string addr)
+{
+    this->_addr.sin_addr.s_addr = inet_addr(INADDR_ANY);
+    if(!addr.empty())
+    {
+
+        struct hostent* h = gethostbyname(addr.c_str());
+        if(h == NULL)
+        {
+            FATAL("addr = `%s` error", addr.c_str());
+            exit(1);
+        }
+        this->_addr.sin_addr = *((struct in_addr*)(h->h_addr_list[0]));
+    }
+        // this->_addr.sin_addr.s_addr = inet_addr(addr.c_str());
+}
+
+uint16_t InetAddr::port() const
+{
+    return ntohs(this->_addr.sin_port);
+}
+
+void InetAddr::port(uint16_t port)
+{
+    this->_addr.sin_port = htons(port);
+}
+
+uint16_t InetAddr::af() const
+{
+    return this->_addr.sin_family;
 }
 
 InetAddr::~InetAddr()
--- include/InetAddr.h
+++ include/InetAddr.h
@@ -1,14 +1,25 @@
 #pragma once
 
 #include <arpa/inet.h>
+#include <string>
 
 class InetAddr
 {
-public:
+private:
+    friend std::string to_string(const InetAddr& );
+    friend std::ostream& operator<<(std::ostream& os, const InetAddr&);
+    friend class Socket;
+
     struct sockaddr_in _addr;
     socklen_t _addr_len;
 
+public:
     InetAddr();
     InetAddr(const char * addr, uint16_t port);
+    std::string addr() const;
+    void addr(std::string addr);
+    uint16_t port() const;
+    void port(uint16_t port);
+    uint16_t af() const;
     ~InetAddr();
 };
