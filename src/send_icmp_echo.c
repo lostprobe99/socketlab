97696a487674dbda11fbaff0af9a7fb1a149775a
lostprobe99 <lostprobe@qq.com>
2025-10-03 08:57:34 +0800

    feat(netpackets): 添加超时控制
    - 新增对接收超时和接收错误处理的处理。使用 poll 机制实现接收超时控制，
    - 定义错误码枚举以明确不同接收状态。

--- src/send_icmp_echo.c
+++ src/send_icmp_echo.c
@@ -8,9 +8,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/poll.h>
 
 #include "netpackets/sock_if.h"
 #include "netpackets/icmp.h"
+#include "netpackets/ip4.h"
 
 #include "simple_log.h"
 
@@ -23,11 +25,34 @@
 #define MAGIC_STR "0123456789abcdefAB"
 #define MAGIC_LEN 18
 
+/*
+type=3, code=0, 网络不可达, 路由表中无路径
+type=3, code=1, 主机不可达, 路由器回复主机不可达
+type=3, code=2, 协议不可达, IP 层已将数据包送达, 但 IP 头中的 协议(protocol) 字段不支持
+type=3, code=3, 端口不可达
+type=3, code=4, 需要分片
+type=11,code=0, TTL超时, 在传输过程中, ttl 减为 0
+**/
+
+enum error_code{
+    RECV_SUCCESS = 0,
+    RECV_TIMEOUT = -1,
+    RECV_ERROR = -2,
+    RECV_BAD_PACKET = -3,
+};
+
 typedef struct __packed _ping_packet_ {
     icmp_hdr_t hdr;
     icmp_echo_t echo;
     char data[MAGIC_LEN];
 } ping_packet_t;
+
+typedef struct recv_packet {
+    uint64_t time_recv;  // 接收时间
+    struct sockaddr_in addr; // 发送方地址
+    ip4_hdr_t ip;   // ip报文头
+    ping_packet_t icmp; // icmp报文
+}recv_data_t;
 
 void usage(char *prog)
 {
@@ -87,19 +112,46 @@ int recv_icmp_echo(int sock, ping_packet_t *recv_pack)
     // ping_packet_t recv_pack = {0};
     char s[MAGIC_LEN + 1] = {0};
     int n = 0, ihl = 0;
+    struct pollfd pfd = {
+        .fd = sock,
+        .events = POLLIN,
+    };
+
+    // 使用 poll 实现等待超时
+    int ret = poll(&pfd, 1, 1000);
+    if ( ret == -1 )
+    {
+        log_error("poll(): %m\n");
+        return RECV_ERROR;
+    }
+    if ( ret == 0 )
+    {
+        log_error("recv timeout\n");
+        return RECV_TIMEOUT;
+    }
 
     n = recvfrom(sock, buf, sizeof(buf), 0, &from, &from_len);
     // 接收响应包
     if(n < 0)
     {
         if(errno == EAGAIN || errno == EWOULDBLOCK)
-            return 0;
+            return RECV_TIMEOUT;
         else
-            return -1;
+        {
+            log_error("recvfrom(): %m\n");
+            return RECV_ERROR;
+        }
     }
+
     log_debug_hexdump("recv buf", buf, n);
     log_debug("接收数据包长度：%d\n", n);
-    log_debug_hexdump("recv buf", buf, n);
+
+    if(n < sizeof(icmp_hdr_t) + sizeof(icmp_echo_t))
+    {
+        // 不完整的 ICMP 包
+        log_error("incomplete ICMP packet\n");
+        return RECV_BAD_PACKET;
+    }
     ihl = buf[0] & 0x0f;    // 提取 ihl
     ihl *= 4;   // 计算 ipv4 头长度
     // buf + ihl skip ipv4 header
@@ -130,12 +182,14 @@ int ping(const char *ip, int n)
 {
     uint64_t start = 0, end = 0;
     ping_packet_t recv_pack;
+    int recv_ret = RECV_SUCCESS;
     int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
     if( sockfd < 0 )
     {
+        // 即使 log level 为 off 也可以执行到 die
         log_die("socket() error: %m\n");
     }
-    log_info("ping %s\n", ip);
+    printf("ping %s\n", ip);
 
     for(int i = 0; i < n; i++)
     {
@@ -143,12 +197,29 @@ int ping(const char *ip, int n)
         log_info("发送时间：%lu\n", start);
         send_icmp_echo(sockfd, ip, i + 1);
 
-        recv_icmp_echo(sockfd, &recv_pack);
+        recv_ret = recv_icmp_echo(sockfd, &recv_pack);
         end = timestamp();
-        log_info("接收时间：%lu, 用时：%lu ms\n", end, end - start);
-        if(recv_pack.hdr.type == ICMP_TYPE_ECHO_REPLY && recv_pack.hdr.code == 0)
+        // 检查接收结果
+        if(recv_ret == RECV_SUCCESS)
         {
-            printf("%s seq: %d, time: %lu ms\n", ip, i + 1, end - start);
+            log_info("接收时间：%lu, 用时：%lu ms\n", end, end - start);
+            // 检查 ICMP 响应包
+            if(recv_pack.hdr.type == ICMP_TYPE_ECHO_REPLY && recv_pack.hdr.code == 0)
+            {
+                printf("from %s: seq=%d, rtt=%lu ms\n", ip, i + 1, end - start);
+            }
+        }
+        else if(recv_ret == RECV_TIMEOUT)
+        {
+            printf("recv timeout, use: %lu ms\n", end - start);
+        }
+        else if(recv_ret == RECV_ERROR)
+        {
+            printf("recv error\n");
+        }
+        else if(recv_ret == RECV_BAD_PACKET)
+        {
+            printf("recv bad packet\n");
         }
     }
 
